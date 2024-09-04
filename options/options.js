cd6b3f21f24ba94fb14bc3624e2ef4e5b747f387
hazzuk <git@hazz.uk>
2024-09-04 22:39:25 +0100

    update files with license header

--- options/options.js
+++ options/options.js
@@ -1,3 +1,16 @@
+/*
+ * This file is licensed under the Affero General Public License (AGPL) version 3.
+ *
+ * Copyright (c) 2024 hazzuk. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, version 3.
+ *
+ * See the GNU Affero General Public License for more details:
+ * <https://www.gnu.org/licenses/agpl-3.0.html>.
+ */
+
 // Function to save the options to storage
 function saveOptions() {
     // Get the values of the toggle boxes
