4fc1141d3c172f301717f4bb4ac81e98bb15e7e8
hazzuk <git@hazz.uk>
2024-09-05 19:21:55 +0100

    add dynamic version/description to options script
    Script will dynamically fetch the extension version/description values from the manifest.

--- options/options.js
+++ options/options.js
@@ -11,18 +11,25 @@
  * <https://www.gnu.org/licenses/agpl-3.0.html>.
  */
 
-// Function to save the options to storage
+
+// display extension version
+document.getElementById("version").textContent = chrome.runtime.getManifest().version;
+// display extension description
+document.getElementById("description").textContent = chrome.runtime.getManifest().description;
+
+
+// function to save the options to storage
 function saveOptions() {
     // Get the values of the toggle boxes
     var optionGGDeals = document.getElementById("option-ggdeals").checked;
     var optionSteam = document.getElementById("option-steam").checked;
 
-    // Save the options to storage
+    // save the options to storage
     chrome.storage.sync.set({
         optionGGDeals: optionGGDeals,
         optionSteam: optionSteam
     }, function () {
-        // Update the context menus based on the options
+        // update the context menus based on the options
         updateContextMenus({
             optionGGDeals: optionGGDeals,
             optionSteam: optionSteam
@@ -30,28 +37,28 @@ function saveOptions() {
     });
 }
 
-// Function to restore the options from storage
+// function to restore the options from storage
 function restoreOptions() {
-    // Set the values of the toggle boxes based on the options, applies defaults if not set
+    // set the values of the toggle boxes based on the options, applies defaults if not set
     chrome.storage.sync.get(["optionGGDeals", "optionSteam"], function (options) {
         document.getElementById("option-ggdeals").checked = options.optionGGDeals || true;
         document.getElementById("option-steam").checked = options.optionSteam || true;
     });
 }
 
-// Function to update the context menus based on the options
+// function to update the context menus based on the options
 function updateContextMenus(options) {
-    // Hide or show "menu-search-ggdeals" context menu
+    // hide or show "menu-search-ggdeals" context menu
     chrome.contextMenus.update("menu-search-ggdeals", {
         visible: options.optionGGDeals
     });
 
-    // Hide or show "menu-search-steam" context menu
+    // hide or show "menu-search-steam" context menu
     chrome.contextMenus.update("menu-search-steam", {
         visible: options.optionSteam
     });
 
-    // Hide or show "menu-elecord" parent context menu
+    // hide or show "menu-elecord" parent context menu
     if (!options.optionGGDeals && !options.optionSteam) {
         chrome.contextMenus.update("menu-elecord", {
             visible: false
@@ -63,9 +70,10 @@ function updateContextMenus(options) {
     }
 }
 
-// Add event listener for when the options are changed
+// add event listener for when the options are changed
 document.getElementById("option-ggdeals").addEventListener("change", saveOptions);
 document.getElementById("option-steam").addEventListener("change", saveOptions);
 
-// Restore the options when the page is loaded
+// restore the options when the page is loaded
 document.addEventListener("DOMContentLoaded", restoreOptions);
+
